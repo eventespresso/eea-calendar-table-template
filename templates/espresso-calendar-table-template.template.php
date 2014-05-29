0961ad318bf5b0c89ce2b387733859621cf4dc3d
Brent Christensen <brent@eventespresso.com>
2014-05-29 16:25:54 -0700

    reanmed file [touch:5963]

--- templates/espresso-calendar-table-template.template.php
+++ templates/espresso-calendar-table-template.template.php
@@ -6,12 +6,11 @@ if ( have_posts() ) :
 	// Start the Loop.
 	while ( have_posts() ) : the_post();
 		// Include the post TYPE-specific template for the content.
-		espresso_get_template_part( 'content', 'espresso_events' );
+		global $post;
+		d( $post );
 	endwhile;
-	// Previous/next page navigation.
-	espresso_pagination();
 	// allow moar other stuff
-	do_action( 'AHEE__archive_espresso_events_template__after_loop' );
+	do_action( 'AHEE__espresso_calendar_table_template_template__after_loop' );
 
 else :
 	// If no content, include the "No posts found" template.
