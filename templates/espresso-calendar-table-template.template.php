9b7244a0b54d92c20efedc80048bfc2d4ea490c6
Seth <seth@eventespresso.com>
2014-05-29 21:35:11 -0600

    Added the table html. [touch:5963 ]

--- templates/espresso-calendar-table-template.template.php
+++ templates/espresso-calendar-table-template.template.php
@@ -3,12 +3,19 @@
 if ( have_posts() ) :
 	// allow other stuff
 	do_action( 'AHEE__espresso_calendar_table_template_template__before_loop' );
+	echo '<table class="cal-table-list">';
 	// Start the Loop.
 	while ( have_posts() ) : the_post();
 		// Include the post TYPE-specific template for the content.
 		global $post;
 		d( $post );
+		echo '<tr class="event-row" id="event-row-'. $post->ID .'">';
+		echo '<td class="td-event-info"><span class="event-title"><a href="'. $post->guid .'">'.$post->post_title.'</a></span>';
+		echo '<p>'.__('When:', 'event_espresso'). ' '. date(get_option('date_format'). ' '.get_option('time_format'), strtotime($post->DTT_EVT_start)). '<br />' .array_shift(explode('<!--more-->', $post->post_content)).'</p>';
+		echo '</td>';
+		echo '</tr>';
 	endwhile;
+	echo '</table>';
 	// allow moar other stuff
 	do_action( 'AHEE__espresso_calendar_table_template_template__after_loop' );
 
