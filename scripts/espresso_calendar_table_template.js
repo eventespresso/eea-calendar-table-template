c6ac9dc6e85314fd5e4e1ef54fa420a3544f89d8
Josh Feck <josh@nxnweb.com>
2018-01-05 19:04:23 -0500

    adjust event handling for when to trigger loading more date times [touch: 5963]

--- scripts/espresso_calendar_table_template.js
+++ scripts/espresso_calendar_table_template.js
@@ -1,22 +1,34 @@
 jQuery(function($){
     var page = 2;
-    var loadmore = 'on';
+    var loadmore = true;
+    $('.cal-table-list').append('<span class="load-more-datetimes"></span>');
+    var trigger = $('.cal-table-list .load-more-datetimes');
+    var scrollHandling = {
+        allow: true,
+        reallow: function() {
+            scrollHandling.allow = true;
+        },
+        delay: 400
+    };
     $(document).on('scroll resize', function() {
-        if ($(window).scrollTop() + $(window).height() + 60 > $(document).height()) {
-            if (loadmore == 'on') {
-                loadmore = 'off';
+        if(loadmore & scrollHandling.allow){
+            scrollHandling.allow = false;
+            setTimeout(scrollHandling.reallow, scrollHandling.delay);
+            var offset = $(trigger).offset().top - $(window).scrollTop();
+            if (1000 > offset) {
+                loadmore = false;
                 $('#lazyload')
                 .append($('<table class="cal-table-list page" id="p' + page + '">')
                 .load(eeCalTable.url + '?calpage=' + page + ' table.page > *', function() {
                     page++;
-                    loadmore = 'on';
+                    loadmore = true;
                 }));
             }
         }
     });
     $(document).ajaxComplete(function( event, xhr, options ) {
         if (xhr.responseText.indexOf('class="event-row"') == -1) {
-            loadmore = 'off';
+            loadmore = false;
         }
     });
 });
